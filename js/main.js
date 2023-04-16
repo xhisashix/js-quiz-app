65b8c317f4392721553ab21a0a7615abfe4db79a
Hisashi Iwamoto <hisasi622@gmail.com>
2023-04-16 15:30:56 +0900

    正誤判定の仕組み変更

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ var quiz = [
       "プレイステーション2",
       "ニンテンドースイッチ",
     ],
-    correct: "ニンテンドースイッチ",
+    correct: 2,
   },
   {
     question:
@@ -17,12 +17,12 @@ var quiz = [
       "プレイステーション2",
       "ニンテンドースイッチ",
     ],
-    correct: "プレイステーション2",
+    correct: 1,
   },
   {
     question: "ファイナルファンタジーⅣの主人公の名前は？",
     answers: ["フリオニール", "クラウド", "ティーダ"],
-    correct: "ティーダ",
+    correct: 2,
   },
 ];
 
@@ -30,24 +30,46 @@ let correctCount = 0; // 正解数をカウントする変数
 let incorrectCount = 0; // 不正解数をカウントする変数
 let quizCount = 0; // 問題数をカウントする変数
 
-// クイズを初期化する
+function sendAnswer() {
+  let result = checkAnswer();
+  showResult(result);
+
+  // 次の問題を表示する
+  quizCount++;
+  compareQuizCount(quizCount);
+  showQuiz(quizCount);
+}
+
+/**
+ * クイズを初期化する
+ */
 function initQuiz() {
   correctCount = 0;
   incorrectCount = 0;
   quizCount = 0;
-  showResult();
   showQuiz(quizCount);
 }
 
-// 正解数と不正回数を表示する
-function showResult() {
+/**
+ *  正解数と不正回数をカウントする
+ * @param {boolean} result - 正誤判定
+ */
+function showResult(result) {
+  if (result) {
+    correctCount++;
+  } else {
+    incorrectCount++;
+  }
   document.getElementById("correct").textContent = `正解数：${correctCount}`;
   document.getElementById(
     "incorrect"
   ).textContent = `不正解数：${incorrectCount}`;
 }
 
-// quizの1問目を表示する
+/**
+ * quizを表示する
+ * @param {number} quizCount - 問題数
+ */
 function showQuiz(quizCount) {
   // 問題文を表示する
   document.getElementById("question").textContent = quiz[quizCount].question;
@@ -55,11 +77,15 @@ function showQuiz(quizCount) {
   let count = quiz[0].answers.length;
   // 選択肢を表示する
   for (let i = 0; i < count; i++) {
-    document.getElementById(`answer${i}`).textContent = quiz[quizCount].answers[i];
+    document.getElementById(`answer${i}`).textContent =
+      quiz[quizCount].answers[i];
   }
 }
 
-// 今の問題と問題数を比較する
+/**
+ * 今の問題と問題数を比較する
+ * @param {number} quizCount - 問題数
+ * */
 function compareQuizCount(quizCount) {
   if (quizCount === quiz.length) {
     // 問題数とquizの数が同じなら終了
@@ -69,22 +95,23 @@ function compareQuizCount(quizCount) {
     showQuiz(quizCount);
   }
 }
-// 正誤判定
-function checkAnswer() {
+
+/**
+ * 正誤判定をする
+ * @param {string} answer - 選択した答え
+ * @return {boolean} 正誤判定
+ */
+function checkAnswer(quizCount) {
   // クリックしたボタンのテキストを取得する
-  let answer = event.target.textContent;
-  console.log(answer);
+  let _answer = document.querySelector("input[name=flexRadioDefault]:checked");
+  let answer = Number(_answer.value)
+  console.log(quiz[0].correct);
   if (answer === quiz[0].correct) {
-    correctCount++;
-    console.log("正解");
+    return true;
   } else {
-    incorrectCount++;
     console.log("不正解");
+    return false;
   }
-  showResult();
-  quizCount++;
-  compareQuizCount(quizCount);
 }
 
-showResult();
-showQuiz(quizCount);
+initQuiz();
